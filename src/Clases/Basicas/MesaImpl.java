01dd8c2046de2f1c2bbef5d8f0df0a391b74012c
Francisco Javier <fjavils200590@gmail.com>
2020-03-06 09:56:55 +0100

    A lot of new methods in class "MesaImpl"

--- src/Clases/Basicas/MesaImpl.java
+++ src/Clases/Basicas/MesaImpl.java
@@ -49,6 +49,7 @@
 
 package Clases.Basicas;
 
+import Clases.Gestoras.GestoraJugadorImpl;
 import Clases.Interfaces.Mesa;
 
 import java.util.ArrayList;
@@ -60,6 +61,8 @@ public class MesaImpl implements Mesa {
     private JugadorImpl[] jugadores;
     private CartaImpl[] cartasMesa;
     private int[][] apuestasJugadores;
+    private int turnoJugador;
+    private int ronda;
 
     /**
      * This constructor places the default attributes
@@ -70,6 +73,8 @@ public class MesaImpl implements Mesa {
         this.jugadores = new JugadorImpl[5];
         this.cartasMesa = new CartaImpl[5];
         this.apuestasJugadores = new int[5][5];
+        this.turnoJugador = 0;
+        this.ronda = 0;
     }
 
     /**
@@ -80,10 +85,13 @@ public class MesaImpl implements Mesa {
      */
 
     public MesaImpl(CartaImpl[] baraja, JugadorImpl[] jugadores, CartaImpl[] cartas){
+        Random random = new Random();
         this.baraja = baraja;
         this.jugadores = jugadores;
         this.cartasMesa = cartas;
         this.apuestasJugadores = new int[5][5];
+        this.turnoJugador = random.nextInt(5);
+        this.ronda = 0;
     }
 
     /**
@@ -267,17 +275,13 @@ public class MesaImpl implements Mesa {
      *                      asignando las cartas a los jugadores de la mesa
      */
 
+    //TODO  Desarrollar javadoc
 
     public void ingresarSaldoGanadores(){
 
+        //TODO
+
     }
-
-
-
-
-
-
-
 
     /*
      * SIGNATURA: public void generarCartasJugadores();
@@ -531,6 +535,154 @@ public class MesaImpl implements Mesa {
 
 
     /*
+     * SIGNATURA: public void realizarApuestas(int turnoJugador, MesaImpl mesa, int ronda)
+     * COMENTARIO: Realizar las apuestas de todos los jugadores de la mesa
+     * PRECONDICIONES:
+     * ENTRADA: - Un entero con el turno
+     *          - Un entero con la ronda
+     * SALIDA: - Nada
+     * ENTRADA/SALIDA: - Un objeto mesa
+     * POSTCONDICIONES: - Modifica el objeto mesa, incrementando el total del dinero que hay en mesa.
+     */
+
+    /*
+     * apuestaMinima = 0
+     * jugadorPasa = false
+     *
+     * si (turnoJugador == 0)
+     *  si (jugador[0].getActivo() == true)
+     *      apuestaJugador = leerApuesta
+     *      si (apuestaJugador > apuestaMinima)
+     *          apuestaMinima = apuestaJugador
+     *      sino
+     *          si (apuestaJugador == apuestaMinima)
+     *              jugadorPasa = true
+     *          finSi
+     *      finSi
+     *  finSi
+     *  turnoJugador++
+     *  cantidadJugadas++
+     *
+     *  mientras(turnoJugador < 5 && cantidadJugadas < totalJugadas)
+     *      si(jugador[turnoJugador].getActivo() == true)
+     *          si(jugador[turnoJugador].getSalto > 0)
+     *              apuestaJugador = calcularApuesta
+     *              si(apuestaJugador != apuestaMinima || jugadorPasa == false)
+     *                  jugadorPasa = false
+     *                  si(apuestaJugador == 0)
+     *                      jugador.seActivo(false)
+     *                      sino
+     *                          si(apuestaJugador == apuestaMinima)
+     *                              jugador.disminuirSaldo(apuestaJugador)
+     *                              mesa.anhadirApuesta(turnoJugador,ronda,apuestaJugador)
+     *                          sino
+     *                              si(apuestaJugador > apuestaMinima)
+     *                                  cantidadJugadas += 4
+     *                                  jugador.disminuirSaldo(apuestaJugador)
+     *                                  mesa.anhadirApuesta(turnoJugador,ronda,apuestaJugador)
+     *                              finSi
+     *                          finSi
+     *                   finSi
+     *              finSi
+     *          finSi
+     *      finSi
+     *      cantidadJugadas++
+     *      si(turnoJugador < 4 && cantidadJugadas < jugadasTotal)
+     *          turnoJugador == 0;
+     *      sino
+     *          turnoJugador++;
+     *      finSi
+     *  finMientras
+     *
+     *
+     */
+
+
+    //TODO Desarrollar javadoc
+    //TODO Documentar codigo mejor
+    //TODO Ver si se puede modular
+    //TODO Disminuir dinero despues de todas las jugadas
+
+    public void realizarApuestas(){
+        GestoraJugadorImpl gesJug = new GestoraJugadorImpl();
+        int apuestaMinima = 0, cantidadJugadas = 0, totalJugadas = 5, apuestaJugador, cantidadJugadoresPasan = 0;
+        boolean jugadorPasa = false;
+        for (JugadorImpl jugador: this.getJugadores()) {
+            if (!jugador.getActivo()){
+                cantidadJugadoresPasan ++;
+            }
+        }
+        if (cantidadJugadoresPasan != 4) {
+            if (this.obtenerJugador(this.turnoJugador).getActivo() && this.obtenerJugador(this.turnoJugador).getSaldo() > 0) {
+                if (this.turnoJugador == 0){
+                    apuestaJugador = gesJug.leerYValidarApuesta(this.obtenerJugador(this.turnoJugador), apuestaMinima);
+                }else{
+                    apuestaJugador = gesJug.calcularApostarBot(apuestaMinima,this,this.turnoJugador,this.ronda);
+                }
+                if (apuestaJugador > apuestaMinima) {
+                    apuestaMinima = apuestaJugador;
+                    this.anhadirApuesta(this.turnoJugador, this.ronda, apuestaMinima);
+                    this.obtenerJugador(this.turnoJugador).disminuirDinero(apuestaMinima);
+                } else {
+                    //Si el jugador apuesta el mismo valor que la apuesta minima (es decir 0) significa que pasa
+                    if (apuestaJugador == apuestaMinima) {
+                        jugadorPasa = true;
+                    }
+                }
+            }
+            cantidadJugadas++;
+            if (this.turnoJugador == 4 && cantidadJugadas < totalJugadas) {
+                this.turnoJugador = 0;
+            } else {
+                this.turnoJugador++;
+            }
+            while (turnoJugador < 5 && cantidadJugadas < totalJugadas) {
+                if (this.obtenerJugador(this.turnoJugador).getActivo() && this.obtenerJugador(this.turnoJugador).getSaldo() > 0) {
+                    if (this.turnoJugador == 0){
+                        apuestaJugador = gesJug.leerYValidarApuesta(this.obtenerJugador(this.turnoJugador),apuestaMinima);
+                    }else{
+                        apuestaJugador = gesJug.calcularApostarBot(apuestaMinima, this, this.turnoJugador,this.ronda);
+                        //TODO En el caso de querer apostar y no poder por no llegar al minimo que haga all-in  (Creo que ya esta solucionado pero tengo que comprobarlo)
+                    }
+                    if (apuestaJugador != apuestaMinima || jugadorPasa) {
+                        jugadorPasa = false;
+                        if (apuestaJugador == 0) {
+                            this.obtenerJugador(this.turnoJugador).setActivo(false);
+                        } else {
+                            if (apuestaJugador > apuestaMinima) {
+                                apuestaMinima = apuestaJugador;
+                                if ((totalJugadas-cantidadJugadas) < 5){
+                                    totalJugadas += 4;
+                                }else{
+                                    totalJugadas++;
+                                }
+                                this.obtenerJugador(this.turnoJugador).disminuirDinero(apuestaJugador);
+                                this.anhadirApuesta(this.turnoJugador, this.ronda, apuestaJugador);
+                            } else {
+                                this.obtenerJugador(this.turnoJugador).disminuirDinero(apuestaJugador);
+                                this.anhadirApuesta(this.turnoJugador, this.ronda, apuestaJugador);
+                            }
+                        }
+                    } else {
+                        this.obtenerJugador(this.turnoJugador).disminuirDinero(apuestaJugador);
+                        this.anhadirApuesta(this.turnoJugador, this.ronda, apuestaJugador);
+                    }
+                }
+                //Actualizamos variables despues de realizar jugada
+                cantidadJugadas++;
+                if (this.turnoJugador == 4 && cantidadJugadas < totalJugadas) {
+                    this.turnoJugador = 0;
+                } else {
+                    this.turnoJugador++;
+                }
+            }
+        }
+        this.ronda++;
+    }
+
+
+
+    /*
      * SIGNATURA: public int[] obtenerGanadores(MesaImpl mesa)
      * COMENTARIO: Calcula quienes son los ganadores de la ronda
      * PRECONDICIONES:
@@ -577,6 +729,127 @@ public class MesaImpl implements Mesa {
         int cantidadMesa;
         cantidadMesa = this.getTotalApuestas();
         this.jugadores[ganador].aumentarDinero(cantidadMesa);
+    }
+
+
+    /*
+     * SIGNATURA: public void ingresarDineroGanador(int ganador, MesaImpl mesa);
+     * COMENTARIO: Aumenta el saldo del usuario ganador de la mano
+     * PRECONDICIONES: - El ganador no puede ser menor de 0 ni mayor de 5
+     * ENTRADA: - Un entero con el ganador
+     * SALIDA: - Nada
+     * ENTRADA/SALIDA: - Un objeto mesa
+     * POSTCONDICIONES: - Modifica el objeto mesa incrementando el saldo del usuario ganador con el total de apuestas de la mano jugada.
+     *
+     */
+
+    //TODO Desarrollar javadoc
+    //TODO Revisar
+
+    public void incrementarTurno(){
+        if (this.turnoJugador == 4){
+            this.turnoJugador = 0;
+        }else{
+            this.turnoJugador++;
+        }
+    }
+
+
+    /*
+     * SIGNATURA: public void mostrarPanelJuego (MesaImpl mesa, int ronda);
+     * COMENTARIO: Imprime por pantalla las cartas que hay en la mesa, el dinero de los demas jugadores y el total del bote de la mesa
+     * PRECONDICIONES: - Nada
+     * ENTRADA: - Un objeto Mesa
+     *          - Un entero
+     * SALIDA: - Nada
+     * ENTRADA/SALIDA: - Nada
+     * POSTCONDICIONES: - No devuelve nada, solo imprime por pantalla el estado actual en el que se encuentran los jugadores y las cartas
+     */
+
+    //TODO Desarrollar javadoc
+    //TODO Utilizar arraybidimensional para esto
+    //TODO Controlar cuando sea la ultima ronda para que muestre informacion adiciona sobre quien ha ganado y mostrar todas las cartas
+    //TODO Implementar imprimir por rondas
+
+    public void mostrarPanelJuego() {
+
+        String numCar1Usuario, numCar2Usuario;
+        char paloCar1Usuario, paloCar2Usuario;
+
+        numCar1Usuario = this.obtenerJugador(0).obtenerCarta(0).getNumero();
+        numCar2Usuario = this.obtenerJugador(0).obtenerCarta(1).getNumero();
+
+        paloCar1Usuario = this.getJugadores()[0].getCartas()[0].getPalo();
+        paloCar2Usuario = this.getJugadores()[0].getCartas()[1].getPalo();
+
+
+        String[] usersJugadores = new String[5];
+        //TODO Revisar esto. Se deberia hacer patron delegacion
+        for (int i = 0; i<usersJugadores.length;i++){
+            usersJugadores[i] = this.getJugadores()[i].getUsuario();
+        }
+
+        int[] saldoJugadores = new int[5];
+        //TODO Revisar esto. Se deberia hacer patron delegacion
+        for (int i = 0; i<saldoJugadores.length;i++){
+            saldoJugadores[i] = this.getJugadores()[i].getSaldo();
+        }
+
+        char[] palosCartasMesa = new char[5];
+        //TODO Revisar esto. Se deberia hacer patron delegacion
+        for (int i = 0; i<palosCartasMesa.length;i++){
+            palosCartasMesa[i] = this.getCartasMesa()[i].getPalo();
+        }
+
+        String[] numerosCartasMesa = new String[5];
+        //TODO Revisar esto. Se deberia hacer patron delegacion
+        for (int i = 0; i<numerosCartasMesa.length;i++){
+            numerosCartasMesa[i] = this.getCartasMesa()[i].getNumero();
+        }
+
+
+        System.out.println("                                      " + usersJugadores[2] + "                                       " + usersJugadores[3]);
+        System.out.println();
+        System.out.println("                                      " + saldoJugadores[2] + "€                                               " + saldoJugadores[3] + "€");
+        System.out.println("                                      |º º|     -----        -----                                    |º º|    -----        -----");
+        System.out.println("                                      |---|    | ?   |      | ?   |                                   |---|   | ?   |      | ?   |  ");
+        System.out.println("                                               | ?   |      | ?   |                                           | ?   |      | ?   |");
+        System.out.println("                                                -----        -----                                             -----        -----");
+
+
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        System.out.println("       " + usersJugadores[1] + "                                                                                                                              " + usersJugadores[4]);
+        System.out.println("                                                        ----- " + "       " + " ----- " + "       " + " ----- " + "       " + " ----- " + "       " + " ----- ");
+        System.out.println("       " + saldoJugadores[1] + "€                                           | " + palosCartasMesa[0] + "   |" + "       " + "| " + palosCartasMesa[1] + "   |" + "       " + "| " + palosCartasMesa[2] + "   |" + "       " + "| " + palosCartasMesa[3] + "   |" + "       " + "| " + palosCartasMesa[4] + "   |                      " + saldoJugadores[4] + "€");
+        System.out.println("       |º º|       -----        -----                  | " + numerosCartasMesa[0] + "   |" + "       " + "| " + numerosCartasMesa[1] + "   |" + "       " + "| " + numerosCartasMesa[2] + "   |" + "       " + "| " + numerosCartasMesa[3] + "   |" + "       " + "| " + numerosCartasMesa[4] + "   |                      |º º|      -----        -----");
+        System.out.println("       |---|      | ?   |      | ?   |                 |     |" + "       " + "|     |" + "       " + "|     |" + "       " + "|     |" + "       " + "|     |" + "                      |---|     | ?   |      | ?   |");
+        System.out.println("                  | ?   |      | ?   |                  ----- " + "       " + " ----- " + "       " + " ----- " + "       " + " ----- " + "       " + " -----                                 |     | ?    | ?   |");
+        System.out.println("                   -----        -----                                                                                                                  -----        -----");
+
+        System.out.println();
+        System.out.println("                                                              DINERO EN MESA: " + this.getTotalApuestas() + "€");
+        System.out.println();
+        System.out.println();
+
+        System.out.println("                                                                     " + usersJugadores[0]);
+        System.out.println("                                                              TU SALDO ES: " + saldoJugadores[0] + "€");
+        System.out.println();
+        System.out.println("                                                           _______________________");
+        System.out.println("                                                          |                       |");
+        System.out.println("                                                          |    __           __    |");
+        System.out.println("                                                          |   |º |         |º |   |            ----------           ----------");
+        System.out.println("                                                          |    --           --    |           | "+paloCar1Usuario+"        |         | "+paloCar2Usuario+"        |");
+        System.out.println("                                                          |                       |           | "+numCar1Usuario+"        |         | "+numCar2Usuario+"        |");
+        System.out.println("                                                          |   \\              /    |           |          |         |          |");
+        System.out.println("                                                          |     \\___________/     |           |          |         |          |");
+        System.out.println("                                                          |                       |            ----------           ----------");
+        System.out.println("                                                           -----------------------");
+        System.out.println();
+
     }
 
 
