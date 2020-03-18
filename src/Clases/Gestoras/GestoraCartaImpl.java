8a2efc97ca711a290bf36bde456b9a793d1c2f31
Fron99 <fjavils200590@gmail.com>
2020-03-18 14:03:48 +0100

    Reorganization of GestoraCartaImpl

--- src/Clases/Gestoras/GestoraCartaImpl.java
+++ src/Clases/Gestoras/GestoraCartaImpl.java
@@ -7,171 +7,7 @@ import java.util.ArrayList;
 
 public class GestoraCartaImpl {
 
-    /*
-     * SIGNATURA: public int evaluarCartas(int jugador, MesaImpl mesa);
-     * COMENTARIO: Calcular el valor que tienen las cartas segun la puntuacion establecida.
-     * PRECONDICIONES: - El entero del jugador debe ser entre 0 y 4
-     *                 - Las cartas del array de la mesa deben estar correlativas y todas las cartas por defecto deben estar detras de las que tienen valor definido.
-     *                 - Las cartas del array de cartas del jugador deben estar correlativas y todas las cartas por defecto deben estar detras de las que tienen valor definido.
-     * ENTRADA: - Un objeto MesaImpl
-     *          - Un entero para el jugador del cual se quiere evaluar los puntos
-     * SALIDA: - Un entero con la puntuacion del jugador
-     * ENTRADA/SALIDA: - Nada
-     * POSTCONDICIONES: - Devuelve asociado al nombre un entero con la puntuacion que tienen las cartas en ese momento del jugador pasado por parametro
-     */
 
-    /**
-     * This method calculates the value of the cards of player passed by parameter in one table
-     * @param jugador player of which we want to evaluate the cards
-     * @param mesa table where the player plays
-     * @return int with the value of the player's cards passed by parameter on a table
-     */
-
-    public int evaluarCartas(int jugador, MesaImpl mesa){
-
-        int puntosFinales, puntosCalculados;
-        CartaImpl[] cartasAEvaluar;
-        GestoraCartaImpl gesCarta = new GestoraCartaImpl();
-
-        //Obtener cartas que se quieren evaluar.
-        cartasAEvaluar = obtenerCartasAEvaluar(jugador,mesa);
-
-        //Ordenar las cartas obtenidas
-        gesCarta.ordenarCartas(cartasAEvaluar);
-
-        //Va comprobando todos los valores de las distintas posibilidades que tiene el jugador y se queda con la posibilidad mas alta.
-        puntosFinales = calcularValorCartaAlta(cartasAEvaluar);
-
-        puntosCalculados = calcularValorPareja(cartasAEvaluar);
-        if (puntosCalculados>puntosFinales){
-            puntosFinales = puntosCalculados;
-        }
-
-        puntosCalculados = calcularValorDoblePareja(cartasAEvaluar);
-        if (puntosCalculados>puntosFinales){
-            puntosFinales = puntosCalculados;
-        }
-
-        puntosCalculados = calcularValorTrio(cartasAEvaluar);
-        if (puntosCalculados>puntosFinales){
-            puntosFinales = puntosCalculados;
-        }
-
-        puntosCalculados = calcularValorEscalera(cartasAEvaluar);
-        if (puntosCalculados>puntosFinales){
-            puntosFinales = puntosCalculados;
-        }
-
-        puntosCalculados = calcularValorColor(cartasAEvaluar);
-        if (puntosCalculados>puntosFinales){
-            puntosFinales = puntosCalculados;
-        }
-
-        puntosCalculados = calcularValorPoker(cartasAEvaluar);
-        if (puntosCalculados>puntosFinales){
-            puntosFinales = puntosCalculados;
-        }
-
-        puntosCalculados = calcularValorFull(cartasAEvaluar);
-        if (puntosCalculados>puntosFinales){
-            puntosFinales = puntosCalculados;
-        }
-
-        puntosCalculados = calcularValorEscaleraColor(cartasAEvaluar);
-        if (puntosCalculados>puntosFinales){
-            puntosFinales = puntosCalculados;
-        }
-        return puntosFinales;
-    }
-
-
-    /*
-     * SIGNATURA: public CartaImpl[] obtenerCartasAEvaluar(int jugador, MesaImpl mesa);
-     * COMENTARIO: Este metodo obtiene el total de cartas que tiene el jugador junto a las disponibles en la mesa para evaluarlas.
-     * PRECONDICIONES: - Nada
-     * ENTRADA: - Un objeto MesaImpl
-     *          - Un entero para el jugador del cual se quiere evaluar los puntos
-     * SALIDA: - Un array de CartaImpl
-     * ENTRADA/SALIDA: - Nada
-     * POSTCONDICIONES: - Devuelve asociado al nombre un array de CartaImpl con las cartas de las que dispone el jugador para evaluar
-     */
-
-    /**
-     * This method get the player's cards to evaluate
-     * @param jugador player of which we want to evaluate the cards
-     * @param mesa table where the player plays
-     * @return CartaImpl[] array of CartaImpl with the player's cards passed by parameter
-     */
-
-
-    private CartaImpl[] obtenerCartasAEvaluar(int jugador, MesaImpl mesa){
-        CartaImpl[] cartasTotales;
-
-        int cantidadCartas = 0;
-        CartaImpl[] cartasMesa = mesa.getCartasMesa();
-        CartaImpl[] cartasJugador = mesa.obtenerJugador(jugador).getCartas();
-
-        //Obtiene la cantidad de cartas que hay en la mesa con valor distinto al de defecto
-        for (int i = 0;i<cartasMesa.length&&(cartasMesa[i].getPalo()!='D');i++){
-            cantidadCartas++;
-        }
-
-        //Obtiene la cantidad de cartas que posee el jugador pasado por parametro con valor distinto al de defecto
-        for (int i = 0;i<cartasJugador.length&&(cartasJugador[i].getPalo()!='D');i++){
-            cantidadCartas++;
-        }
-
-        cartasTotales = new CartaImpl[cantidadCartas];
-
-        //Inicializar el array para no tener errores de null
-        for (int i = 0; i<cartasTotales.length;i++){
-            cartasTotales[i] = new CartaImpl();
-        }
-
-        //Anhade al array de cartas totales todas las cartas distintas al valor por defecto que hay en la mesa
-        for (int i = 0;i<cartasMesa.length&&(cartasMesa[i].getPalo()!='D');i++){
-            cartasTotales[i] = cartasMesa[i];
-        }
-
-        //Anhade al array de cartas totales todas las cartas distintas al valor por defecto que posee el jugador
-        for (int i = 0,j = 0;i<cartasTotales.length;i++){
-            if (cartasTotales[i].getPalo()=='D'){
-                cartasTotales[i] = cartasJugador[j];
-                j++;
-            }
-        }
-        return cartasTotales;
-    }
-
-
-    /*
-     * SIGNATURA: public void ordenarCartas(CartaImpl[] cartas);
-     * COMENTARIO: Ordena las cartas de un array de cartas.
-     * PRECONDICIONES: - El array debe tener como minimo 2 cartas
-     * ENTRADA: - Nada
-     * SALIDA: - Nada
-     * ENTRADA/SALIDA: - Un array de CartaImpl
-     * POSTCONDICIONES: - Modifica el array pasado por parametro ordenado las cartas que contiene de mayor a menor valor.
-     */
-
-    /**
-     * This method sort the array passed by parameter
-     * @param cartas Array of CartaImpl[] you want to order
-     */
-
-    public void ordenarCartas(CartaImpl[] cartas){
-        CartaImpl cartaAux;
-        //Para ordenar utiliza el metodo de compareTo de la clase CartaImpl
-        for(int i = 0; i < cartas.length - 1; i++){
-            for(int j = 0; j < cartas.length - 1; j++){
-                if (cartas[j].compareTo(cartas[j + 1]) == 1){
-                    cartaAux = cartas[j+1];
-                    cartas[j+1] = cartas[j];
-                    cartas[j] = cartaAux;
-                }
-            }
-        }
-    }
 
 
     /*
@@ -830,6 +666,173 @@ public class GestoraCartaImpl {
 
         }
         return puntos;
+    }
+
+
+    /*
+     * SIGNATURA: public int evaluarCartas(int jugador, MesaImpl mesa);
+     * COMENTARIO: Calcular el valor que tienen las cartas segun la puntuacion establecida.
+     * PRECONDICIONES: - El entero del jugador debe ser entre 0 y 4
+     *                 - Las cartas del array de la mesa deben estar correlativas y todas las cartas por defecto deben estar detras de las que tienen valor definido.
+     *                 - Las cartas del array de cartas del jugador deben estar correlativas y todas las cartas por defecto deben estar detras de las que tienen valor definido.
+     * ENTRADA: - Un objeto MesaImpl
+     *          - Un entero para el jugador del cual se quiere evaluar los puntos
+     * SALIDA: - Un entero con la puntuacion del jugador
+     * ENTRADA/SALIDA: - Nada
+     * POSTCONDICIONES: - Devuelve asociado al nombre un entero con la puntuacion que tienen las cartas en ese momento del jugador pasado por parametro
+     */
+
+    /**
+     * This method calculates the value of the cards of player passed by parameter in one table
+     * @param jugador player of which we want to evaluate the cards
+     * @param mesa table where the player plays
+     * @return int with the value of the player's cards passed by parameter on a table
+     */
+
+    public int evaluarCartas(int jugador, MesaImpl mesa){
+
+        int puntosFinales, puntosCalculados;
+        CartaImpl[] cartasAEvaluar;
+        GestoraCartaImpl gesCarta = new GestoraCartaImpl();
+
+        //Obtener cartas que se quieren evaluar.
+        cartasAEvaluar = obtenerCartasAEvaluar(jugador,mesa);
+
+        //Ordenar las cartas obtenidas
+        gesCarta.ordenarCartas(cartasAEvaluar);
+
+        //Va comprobando todos los valores de las distintas posibilidades que tiene el jugador y se queda con la posibilidad mas alta.
+        puntosFinales = calcularValorCartaAlta(cartasAEvaluar);
+
+        puntosCalculados = calcularValorPareja(cartasAEvaluar);
+        if (puntosCalculados>puntosFinales){
+            puntosFinales = puntosCalculados;
+        }
+
+        puntosCalculados = calcularValorDoblePareja(cartasAEvaluar);
+        if (puntosCalculados>puntosFinales){
+            puntosFinales = puntosCalculados;
+        }
+
+        puntosCalculados = calcularValorTrio(cartasAEvaluar);
+        if (puntosCalculados>puntosFinales){
+            puntosFinales = puntosCalculados;
+        }
+
+        puntosCalculados = calcularValorEscalera(cartasAEvaluar);
+        if (puntosCalculados>puntosFinales){
+            puntosFinales = puntosCalculados;
+        }
+
+        puntosCalculados = calcularValorColor(cartasAEvaluar);
+        if (puntosCalculados>puntosFinales){
+            puntosFinales = puntosCalculados;
+        }
+
+        puntosCalculados = calcularValorPoker(cartasAEvaluar);
+        if (puntosCalculados>puntosFinales){
+            puntosFinales = puntosCalculados;
+        }
+
+        puntosCalculados = calcularValorFull(cartasAEvaluar);
+        if (puntosCalculados>puntosFinales){
+            puntosFinales = puntosCalculados;
+        }
+
+        puntosCalculados = calcularValorEscaleraColor(cartasAEvaluar);
+        if (puntosCalculados>puntosFinales){
+            puntosFinales = puntosCalculados;
+        }
+        return puntosFinales;
+    }
+
+
+    /*
+     * SIGNATURA: public CartaImpl[] obtenerCartasAEvaluar(int jugador, MesaImpl mesa);
+     * COMENTARIO: Este metodo obtiene el total de cartas que tiene el jugador junto a las disponibles en la mesa para evaluarlas.
+     * PRECONDICIONES: - Nada
+     * ENTRADA: - Un objeto MesaImpl
+     *          - Un entero para el jugador del cual se quiere evaluar los puntos
+     * SALIDA: - Un array de CartaImpl
+     * ENTRADA/SALIDA: - Nada
+     * POSTCONDICIONES: - Devuelve asociado al nombre un array de CartaImpl con las cartas de las que dispone el jugador para evaluar
+     */
+
+    /**
+     * This method get the player's cards to evaluate
+     * @param jugador player of which we want to evaluate the cards
+     * @param mesa table where the player plays
+     * @return CartaImpl[] array of CartaImpl with the player's cards passed by parameter
+     */
+
+
+    private CartaImpl[] obtenerCartasAEvaluar(int jugador, MesaImpl mesa){
+        CartaImpl[] cartasTotales;
+
+        int cantidadCartas = 0;
+        CartaImpl[] cartasMesa = mesa.getCartasMesa();
+        CartaImpl[] cartasJugador = mesa.obtenerJugador(jugador).getCartas();
+
+        //Obtiene la cantidad de cartas que hay en la mesa con valor distinto al de defecto
+        for (int i = 0;i<cartasMesa.length&&(cartasMesa[i].getPalo()!='D');i++){
+            cantidadCartas++;
+        }
+
+        //Obtiene la cantidad de cartas que posee el jugador pasado por parametro con valor distinto al de defecto
+        for (int i = 0;i<cartasJugador.length&&(cartasJugador[i].getPalo()!='D');i++){
+            cantidadCartas++;
+        }
+
+        cartasTotales = new CartaImpl[cantidadCartas];
+
+        //Inicializar el array para no tener errores de null
+        for (int i = 0; i<cartasTotales.length;i++){
+            cartasTotales[i] = new CartaImpl();
+        }
+
+        //Anhade al array de cartas totales todas las cartas distintas al valor por defecto que hay en la mesa
+        for (int i = 0;i<cartasMesa.length&&(cartasMesa[i].getPalo()!='D');i++){
+            cartasTotales[i] = cartasMesa[i];
+        }
+
+        //Anhade al array de cartas totales todas las cartas distintas al valor por defecto que posee el jugador
+        for (int i = 0,j = 0;i<cartasTotales.length;i++){
+            if (cartasTotales[i].getPalo()=='D'){
+                cartasTotales[i] = cartasJugador[j];
+                j++;
+            }
+        }
+        return cartasTotales;
+    }
+
+
+    /*
+     * SIGNATURA: public void ordenarCartas(CartaImpl[] cartas);
+     * COMENTARIO: Ordena las cartas de un array de cartas.
+     * PRECONDICIONES: - El array debe tener como minimo 2 cartas
+     * ENTRADA: - Nada
+     * SALIDA: - Nada
+     * ENTRADA/SALIDA: - Un array de CartaImpl
+     * POSTCONDICIONES: - Modifica el array pasado por parametro ordenado las cartas que contiene de mayor a menor valor.
+     */
+
+    /**
+     * This method sort the array passed by parameter
+     * @param cartas Array of CartaImpl[] you want to order
+     */
+
+    public void ordenarCartas(CartaImpl[] cartas){
+        CartaImpl cartaAux;
+        //Para ordenar utiliza el metodo de compareTo de la clase CartaImpl
+        for(int i = 0; i < cartas.length - 1; i++){
+            for(int j = 0; j < cartas.length - 1; j++){
+                if (cartas[j].compareTo(cartas[j + 1]) == 1){
+                    cartaAux = cartas[j+1];
+                    cartas[j+1] = cartas[j];
+                    cartas[j] = cartaAux;
+                }
+            }
+        }
     }
 
 
