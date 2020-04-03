d064f228d677bb6b7e87b85255b576d5294f51c5
Fron99 <fjavils200590@gmail.com>
2020-04-03 20:14:18 +0200

    Bug fixed in Validaciones

--- src/Validaciones/Validaciones.java
+++ src/Validaciones/Validaciones.java
@@ -23,7 +23,7 @@ public class Validaciones {
         Scanner te = new Scanner(System.in);
         char caracter;
             do {
-                System.out.println("Introduce 'S' si desea seguir jugando o 'N' si desea salir de la mesa: ");
+                System.out.print("Introduce 'S' si desea seguir jugando o 'N' si desea salir de la mesa: ");
                 caracter = Character.toUpperCase(te.next().charAt(0));
             }while (caracter != 'S' && caracter != 'N');
         return caracter == 'S';
