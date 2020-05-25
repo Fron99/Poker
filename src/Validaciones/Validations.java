30fb69f88a53e1e78da1f96080350bab8af9048d
Francisco Javier <fjavils200590@gmail.com>
2020-05-25 21:04:13 +0200

    New method in Validations

--- src/Validaciones/Validations.java
+++ src/Validaciones/Validations.java
@@ -4,29 +4,35 @@ import java.util.Scanner;
 
 public class Validations {
 
-    /*
-     * SIGNATURA:
-     * COMENTARIO:
-     * PRECONDICIONES:
-     * ENTRADA: -
-     *          -
-     * SALIDA: -
-     * ENTRADA/SALIDA: -
-     * POSTCONDICIONES: -
+    /**
+     * @return
      */
+
+    public boolean readAndValidateContinuePlaying(){
+        Scanner sc = new Scanner(System.in);
+        char caracter;
+            do {
+                System.out.print("Insert 'S' if want continue playing or 'N' if want leave of table game: ");
+                caracter = Character.toUpperCase(sc.next().charAt(0));
+            }while (caracter != 'S' && caracter != 'N');
+        return caracter == 'S';
+    }
 
     /**
      * @return
      */
 
-    public boolean leerYValidarSeguirJugando(){
-        Scanner te = new Scanner(System.in);
-        char caracter;
-            do {
-                System.out.print("Introduce 'S' si desea seguir jugando o 'N' si desea salir de la mesa: ");
-                caracter = Character.toUpperCase(te.next().charAt(0));
-            }while (caracter != 'S' && caracter != 'N');
-        return caracter == 'S';
+    public int readAndValidateGame(){
+        Scanner sc = new Scanner(System.in);
+        int option;
+        do {
+            System.out.println("0.- Leave");
+            System.out.println("1.- Play poker");
+            System.out.println("2.- Play roulette");
+            System.out.print("Insert option: ");
+            option = sc.nextInt();
+        }while (option < 0 || option > 2);
+        return option;
     }
 
 }
