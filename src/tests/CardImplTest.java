a509b2c3e1715a1a110af55e30b186425013eb14
Francisco Javier <fjavils200590@gmail.com>
2020-06-03 20:39:04 +0200

    Tests created for CardImpl, Gender and PlayerImpl

---
+++ src/tests/CardImplTest.java
@@ -0,0 +1,40 @@
+package tests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CardImplTest {
+
+    @Test
+    public void getSuit() {
+    }
+
+    @Test
+    public void getNumber() {
+    }
+
+    @Test
+    public void getValueNumber() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+
+    @Test
+    public void testHashCode() {
+    }
+
+    @Test
+    public void compareTo() {
+    }
+
+    @Test
+    public void testEquals() {
+    }
+
+    @Test
+    public void testClone() {
+    }
+}
---
+++ src/tests/GendersTest.java
@@ -0,0 +1,25 @@
+package tests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GendersTest {
+
+    @Test
+    public void readAndValidateGender() {
+    }
+
+    @Test
+    public void readAndShowGender() {
+    }
+
+    @Test
+    public void values() {
+    }
+
+    @Test
+    public void valueOf() {
+    }
+
+}
